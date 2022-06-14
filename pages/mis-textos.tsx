5021b9c18d608a9b6e4a28562de27c094516195d
Manuel Serrano Scholz <mserrano@surcontrol.com>
2022-06-14 10:19:23 +0200

    Feat: Cargar los PDFs correspondientes a cada usuario

--- pages/mis-textos.tsx
+++ pages/mis-textos.tsx
@@ -1,24 +1,48 @@
 import { supabase } from '@/utils/supabase-client';
-import { useEffect } from 'react';
+import { withAuthRequired } from '@supabase/supabase-auth-helpers/nextjs';
+import { User } from '@supabase/supabase-auth-helpers/react';
+import { useEffect, useState } from 'react';
 
-export default function Account() {
+export interface Text {
+    text_title: string | undefined;
+    text: string | undefined;
+}
+
+export default function Texts({ user }: { user: User }) {
+    const initialValue: Text[] = [
+        {
+            text_title: "Cargando...",
+            text: "/"
+        }
+    ]
+
+    const [text, setTexts] = useState<Text[]>(initialValue);
 
     useEffect(() => {
         getTexts();
-    }, [])
+    }, [user])
+
 
     async function getTexts() {
-        const { data, error } = await supabase.from('textos').select();
-
-        console.log(data);
+        const {data, error}: {data: any; error: any} = await supabase.from('textos').select('text, text_title').eq("user_id", user.id);
+        setTexts(data);
     }
 
     return (
         <>
             <div>
-                <p></p>
+                {
+                    text && text.map(pdf => {
+                        console.log(pdf.text_title);
+                        return (
+                            <a download={pdf.text_title !== null ? pdf.text_title : "PDF"} href={pdf.text} title='Descargar PDF'>Descargar {pdf.text_title !== null ? pdf.text_title : "PDF sin nombre (Ponte en contacto con soporte@dominio.com"}</a>
+                        )
+                    })
+                }
             </div>
         </>
     )
 
 }
+
+export const getServerSideProps = withAuthRequired({ redirectTo: '/signin' });
