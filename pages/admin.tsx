f84278e4be2cc3fd8ca8da8eb0a4952af7716fa7
Manuel Scholz <manuel.serrano943@gmail.com>
2022-06-14 16:58:03 +0200

    Refactor: Ordenar funciones en admin.tsx

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -7,6 +7,58 @@ export default function Account({ users }: any) {
     const [selectedRequest, setSelectedRequest] = useState<any>(null);
     const [requests, setRequests] = useState<any>(null);
 
+    // Carga los usuarios cuando recibe el objeto users
+    useEffect(() => {
+        let select = document.getElementById("upload-file-uuids")! as HTMLSelectElement;
+        users.forEach((el: { full_name: any; id: string; }) => {
+            let option = document.createElement("option");
+            option.textContent = `${el.full_name} con el ID: ${el.id}`;
+            option.value = el.id;
+            select.appendChild(option);
+        })
+    }, [users])
+
+    // Si ha escogido un usuario, se cargan los requests
+    useEffect(() => {
+        if (userId) {
+            getRequests();
+        }
+    }, [userId])
+
+    // Obtiene las requests y las agrega al state
+    async function getRequests() {
+        await supabase.from('requests').select().eq("user_id", userId).eq("finished", false).then((requests) => {
+            setRequests(requests.body)
+        });
+    }
+
+    // Cuando los requests se han cargado y son mas de 0, se muestran en el selector
+    useEffect(() => {
+        if (requests && requests.length > 0) {
+            let select = document.getElementById("upload-file-req-ids")! as HTMLSelectElement;
+            let option = document.createElement("option");
+            option.textContent = `Elige un usuario`;
+            select.appendChild(option);
+            requests.forEach((el: { title: any; id: string; user_id: string }) => {
+                let option = document.createElement("option");
+                option.textContent = `Petición: ${el.title} para el usuario: ${el.user_id}`;
+                option.value = el.id;
+                select.appendChild(option);
+            })
+        }
+    }, [requests])
+
+    // Agrega el usuario escogido al state y limpia el selector de requests
+    function setSelectedUser(e: any) {
+        removeChildElements(document.getElementById("upload-file-req-ids")!);
+        setUserId(e.target.value);
+    }
+
+    // Agrega la requests elegida al state
+    function setSelectedReq(e: any) {
+        setSelectedRequest(e.target.value);
+    }
+
     // Función encargada de actualizar la request con el PDF y en estado finalizado
     async function onSubmit(e: any) {
         e.preventDefault();
@@ -30,58 +82,6 @@ export default function Account({ users }: any) {
             }
         }
     }
-
-    // Agrega el usuario escogido al state y limpia el selector de requests
-    function setSelectedUser(e: any) {
-        removeChildElements(document.getElementById("upload-file-req-ids")!);
-        setUserId(e.target.value);
-    }
-
-    // Agrega la requests elegida al state
-    function setSelectedReq(e: any) {
-        setSelectedRequest(e.target.value);
-    }
-
-    // Obtiene las requests y las agrega al state
-    async function getRequests() {
-        await supabase.from('requests').select().eq("user_id", userId).eq("finished", false).then((requests) => {
-            setRequests(requests.body)
-        });
-    }
-
-    // Carga los usuarios cuando recibe el objeto users
-    useEffect(() => {
-        let select = document.getElementById("upload-file-uuids")! as HTMLSelectElement;
-        users.forEach((el: { full_name: any; id: string; }) => {
-            let option = document.createElement("option");
-            option.textContent = `${el.full_name} con el ID: ${el.id}`;
-            option.value = el.id;
-            select.appendChild(option);
-        })
-    }, [users])
-
-    // Si ha escogido un usuario, se cargan los requests
-    useEffect(() => {
-        if (userId) {
-            getRequests();
-        }
-    }, [userId])
-
-    // Cuando los requests se han cargado y son mas de 0, se muestran en el selector
-    useEffect(() => {
-        if (requests && requests.length > 0) {
-            let select = document.getElementById("upload-file-req-ids")! as HTMLSelectElement;
-            let option = document.createElement("option");
-            option.textContent = `Elige un usuario`;
-            select.appendChild(option);
-            requests.forEach((el: { title: any; id: string; user_id: string }) => {
-                let option = document.createElement("option");
-                option.textContent = `Petición: ${el.title} para el usuario: ${el.user_id}`;
-                option.value = el.id;
-                select.appendChild(option);
-            })
-        }
-    }, [requests])
 
     return (
         <>
