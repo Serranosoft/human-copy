1666cac51e20eed9fa2ec48f2bc675e2794c000c
Manuel Scholz <manuel.serrano943@gmail.com>
2022-06-15 19:39:18 +0200

    Refactor: Almacenar en variables la info. de los planes en Pricing

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,4 +1,3 @@
-import cn from 'classnames';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
@@ -12,16 +11,10 @@ interface Props {
     products: ProductWithPrice[];
 }
 
-type BillingInterval = 'year' | 'month';
-
 export default function Pricing({ products }: Props) {
     const router = useRouter();
-    const [billingInterval, setBillingInterval] =
-        useState<BillingInterval>('month');
     const [priceIdLoading, setPriceIdLoading] = useState<string>();
     const { user, isLoading, subscription } = useUser();
-
-
     const [range, applyRange] = useState(0);
     const [productPrice, applyProductPrice] = useState<number | undefined>(10);
 
@@ -35,7 +28,6 @@ export default function Pricing({ products }: Props) {
         }
 
         try {
-            console.log(mode);
             const { sessionId } = await postData({
                 url: '/api/create-checkout-session',
                 data: { price, mode }
@@ -68,6 +60,9 @@ export default function Pricing({ products }: Props) {
             />
             <span>{productPrice}</span>
             {products.map((product) => { 
+                let id = product.prices!.length > 1 ? product.prices![range].id : product.prices![0].id;
+                let price = product.prices!.length > 1 ? product.prices![range] : product.prices![0];
+                let mode = product.prices!.length > 1 ? "payment" : "subscription";
                 return (
                     <div>
                         <span>{product.name}</span>
@@ -76,9 +71,9 @@ export default function Pricing({ products }: Props) {
                         <Button
                             type="button"
                             disabled={isLoading}
-                            /* loading={priceIdLoading === product.prices!.length > 1 ? product.prices![range].id : product.prices![0].id} */
-                            onClick={() => handleCheckout(product.prices!.length > 1 ? product.prices![range] : product.prices![0], product.prices!.length > 1 ? "payment" : "subscription")}
-                            className="mt-8 block w-full rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-zinc-900"
+                            loading={priceIdLoading === id}
+                            onClick={() => handleCheckout(price, mode)}
+                            className=""
                         >
                             {product.name === subscription?.prices?.products?.name
                                 ? 'Gestionar suscripción'
@@ -89,85 +84,4 @@ export default function Pricing({ products }: Props) {
             })}
         </section>
     )
-    {/* <section className="bg-black">
-            <div className="max-w-6xl mx-auto py-8 sm:py-24 px-4 sm:px-6 lg:px-8">
-                <div className="sm:flex sm:flex-col sm:align-center">
-                    <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-                        Precios
-                    </h1>
-                    <p className="mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl max-w-2xl m-auto">
-                        Regístrate y contrata una suscripción para solicitar textos. La suscripción anual ofrece un 20% de Dto.</p>
-                    <div className="relative self-center mt-6 bg-zinc-900 rounded-lg p-0.5 flex sm:mt-8 border border-zinc-800">
-                        <button
-                            onClick={() => setBillingInterval('month')}
-                            type="button"
-                            className={`${billingInterval === 'month'
-                                ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                                : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
-                        >
-                            Suscripción mensual
-                        </button>
-                        <button
-                            onClick={() => setBillingInterval('year')}
-                            type="button"
-                            className={`${billingInterval === 'year'
-                                ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                                : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
-                        >
-                            Suscripción anual
-                        </button>
-                    </div>
-                </div>
-                <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
-                    {products.map((product) => {
-                        const price = product.prices?.find(
-                            (price) => price.interval === billingInterval
-                        );
-                        if (!price) return;
-                        return (
-                            <div
-                                key={product.id}
-                                className={cn(
-                                    'rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900',
-                                    {
-                                        'border border-pink-500': subscription
-                                            ? product.name === subscription?.prices?.products?.name
-                                            : product.name === 'Freelancer'
-                                    }
-                                )}
-                            >
-                                <div className="p-6">
-                                    <h2 className="text-2xl leading-6 font-semibold text-white">
-                                        {product.name}
-                                    </h2>
-                                    <p className="mt-4 text-zinc-300">{product.description}</p>
-                                    <p className="mt-8">
-                                        <span className="text-5xl font-extrabold white">
-                                            {price.unit_amount! / 100}€
-                                        </span>
-                                        <span className="text-base font-medium text-zinc-100">
-                                            /{billingInterval === "month" ? "mes" : "año"}
-                                        </span>
-                                    </p>
-                                    <Button
-                                        type="button"
-                                        disabled={isLoading}
-                                        loading={priceIdLoading === price!.id}
-                                        onClick={() => handleCheckout(price!)}
-                                        className="mt-8 block w-full rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-zinc-900"
-                                    >
-                                        {product.name === subscription?.prices?.products?.name
-                                            ? 'Gestionar suscripción'
-                                            : 'Suscribirse'}
-                                    </Button>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-        </section> */}
-
 }
