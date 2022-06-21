2429ace4dae629ee1fd2b0e8a890e43d1667a80b
Manuel Scholz <manuel.serrano943@gmail.com>
2022-06-22 00:15:37 +0200

    WIP: Commit anterior

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -110,7 +110,8 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
                         } else {
                             // Sumar el plan que va a comprar al que ya tiene (si ya tiene alguno, claro)
                             let resultPlan = plan + currentPlan;
-                            let pruebawtf = plan;
+                            // plan: ok, cualquier otro valor: ok, temp: currentPlan, plan+currentPlan: fallo.
+                            let pruebawtf = currentPlan;
                             await supabase.from('users').update({plan: pruebawtf}).match({ email: checkoutSession.customer_details!.email });
                         }
                         break;
