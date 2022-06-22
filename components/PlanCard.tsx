c091ee8591641104913bc9b80e0c6ec9d339f502
Manuel Serrano Scholz <mserrano@surcontrol.com>
2022-06-22 13:31:05 +0200

    Feat: Agregar mas precios al plan de palabras

--- components/PlanCard.tsx
+++ components/PlanCard.tsx
@@ -5,22 +5,19 @@ import Range from './ui/Range';
 
 interface Props {
     product: any;
-    /* productPrice?: number; */
     isLoading: boolean;
     priceIdLoading?: string;
     handleCheckout: any;
     buttonText: string;
-    /* priceId: string; */
     paymentType: string;
-    /* wordsPerPrice: number */
 }
 
-export default function PlanCard({ product, /* productPrice,  */isLoading, priceIdLoading, handleCheckout, buttonText, /* priceId, */ paymentType, /* wordsPerPrice */ }: Props) {
+export default function PlanCard({ product, isLoading, priceIdLoading, handleCheckout, buttonText, paymentType}: Props) {
 
     const [price, setPrice] = useState<any>();
-    const [range, applyRange] = useState(1);
-    const [productPrice, applyProductPrice] = useState<number | undefined>(45);
-    const [wordsPerPrice, setWordsPerPrice] = useState(4000);
+    const [range, applyRange] = useState(5);
+    const [productPrice, applyProductPrice] = useState<number | undefined>(50);
+    const [wordsPerPrice, setWordsPerPrice] = useState(5000);
     const [priceId, setPriceId] = useState();
 
     useEffect(() => {
@@ -34,7 +31,6 @@ export default function PlanCard({ product, /* productPrice,  */isLoading, price
     }, [productPrice])
 
     function handleChange(e: any) {
-        console.log(e.target.value);
         applyRange(e.target.value)
         applyProductPrice(product!.prices![e.target.value].unit_amount! / 100);
     }
@@ -42,14 +38,35 @@ export default function PlanCard({ product, /* productPrice,  */isLoading, price
     useEffect(() => {
         if (productPrice) {
             switch (productPrice) {
+                case 10:
+                    setWordsPerPrice(1000);
+                    break;
+                case 20:
+                    setWordsPerPrice(2000);
+                    break;
                 case 30:
                     setWordsPerPrice(3000);
                     break;
-                case 45:
+                case 40:
                     setWordsPerPrice(4000);
                     break;
-                case 60:
+                case 50:
                     setWordsPerPrice(5000);
+                    break;
+                case 60:
+                    setWordsPerPrice(6000);
+                    break;
+                case 70:
+                    setWordsPerPrice(7000);
+                    break;
+                case 80:
+                    setWordsPerPrice(8000);
+                    break;
+                case 90:
+                    setWordsPerPrice(9000);
+                    break;
+                case 100:
+                    setWordsPerPrice(10000);
                     break;
             }
         }
@@ -69,12 +86,12 @@ export default function PlanCard({ product, /* productPrice,  */isLoading, price
                             <span>1000</span>
                             <Range
                                 min="0"
-                                max="2"
+                                max="9"
                                 value={range}
                                 onChange={handleChange}
                                 step="1"
                             />
-                            <span>6000</span>
+                            <span>10000</span>
                         </div>
                     </>
                 }
