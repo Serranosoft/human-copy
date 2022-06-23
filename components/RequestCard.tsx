5ce3a9949b9f0d69b5e8688e5feb32518aa0de60
Manuel Serrano Scholz <mserrano@surcontrol.com>
2022-06-23 22:13:38 +0200

    WIP: Cambio estilos requestCard

--- components/RequestCard.tsx
+++ components/RequestCard.tsx
@@ -1,4 +1,4 @@
-import s from '../styles/css/Mis-textos.module.css';
+import s from '../styles/css/RequestCard.module.css';
 import LoadingDots from '@/components/ui/LoadingDots';
 
 export interface Request {
@@ -11,23 +11,30 @@ export interface Request {
 }
 
 export default function RequestCard({request}: { request: Request }) {
-
+    // https://www.eleken.co/blog-posts/card-ui-examples-and-best-practices-for-product-owners
     return (
-        <div>
-            <span className={s.title}>{request.title !== null ? request.title : "(HumanCopy redactará un título)"}</span>
-            <span className={s.words}>{request.words} palabras</span>
-            <span className={s.description}>{request.description}</span>
-            {
-                request.download !== null && request.download !== "" ?
-                    <a className={s.download} download={request.title !== null ? request.title : "Sin nombre"} href={request.download} title={`Descargar ${request.title} en PDF`}>Descargar</a>
-                    :
-                    <div className={s.download}>
-                        <span>Pendiente</span>
-                        <i>
-                            <LoadingDots />
-                        </i>
-                    </div>
-            }
+        <div className={s.root}>
+            <div className={s.cardBody}>
+                <span>Fecha de entrega estimada: 28/06/22</span>
+                <span className={s.title}>{request.title !== null ? request.title : "(HumanCopy redactará un título)"}</span>
+                <span className={s.words}>{request.words} palabras</span>
+                <span className={s.description}>{request.description}</span>
+            </div>
+            <img src="" className={s.separator} />
+            <div className={s.cardFooter}>
+                {
+                    request.download !== null && request.download !== "" ?
+                        <a className={s.download} download={request.title !== null ? request.title : "Sin nombre"} href={request.download} title={`Descargar ${request.title} en PDF`}>Descargar</a>
+                        :
+                        <div className={s.download}>
+                            <span>Pendiente</span>
+                            <i>
+                                <LoadingDots />
+                            </i>
+                        </div>
+                }
+            </div>
+        
         </div>
     )
 }
