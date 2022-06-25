ad22334e0ee50e19ecd609566765c03e4f04209d
Manuel Scholz <manuel.serrano943@gmail.com>
2022-06-25 18:19:30 +0200

    Feat: Diseño de las request cards

--- components/RequestCard.tsx
+++ components/RequestCard.tsx
@@ -1,5 +1,6 @@
 import s from '../styles/css/RequestCard.module.css';
 import LoadingDots from '@/components/ui/LoadingDots';
+import Button from './ui/Button';
 
 export interface Request {
     finished: boolean | undefined;
@@ -11,30 +12,30 @@ export interface Request {
 }
 
 export default function RequestCard({request}: { request: Request }) {
-    // https://www.eleken.co/blog-posts/card-ui-examples-and-best-practices-for-product-owners
     return (
         <div className={s.root}>
-            <div className={s.cardBody}>
-                <span>Fecha de entrega estimada: 28/06/22</span>
-                <span className={s.title}>{request.title !== null ? request.title : "(HumanCopy redactará un título)"}</span>
-                <span className={s.words}>{request.words} palabras</span>
-                <span className={s.description}>{request.description}</span>
-            </div>
-            <img src="" className={s.separator} />
-            <div className={s.cardFooter}>
+            <div className={s.header}>
+                <div style={{border: request.download !== null && request.download !== "" ? "1px solid #16c60c" : "1px solid rgb(73, 73, 73);"}}>
                 {
                     request.download !== null && request.download !== "" ?
-                        <a className={s.download} download={request.title !== null ? request.title : "Sin nombre"} href={request.download} title={`Descargar ${request.title} en PDF`}>Descargar</a>
+                        <span>✔️</span>
                         :
-                        <div className={s.download}>
-                            <span>Pendiente</span>
-                            <i>
-                                <LoadingDots />
-                            </i>
-                        </div>
+                        <i>
+                            <LoadingDots />
+                        </i>
                 }
+                </div>
+                <p className={s.date}><span>28-06-22</span><span> /aprox.</span></p>
             </div>
-        
+            <div className={s.info}>
+                <p className={s.title}>{request.title !== null ? request.title : "(HumanCopy redactará un título)"}</p>
+                <p className={s.words}>{request.words} palabras</p>
+            </div>
+            <div className={s.description}>
+                <p>Descripción</p>
+                <p>{request.description}</p>
+            </div>
+            <Button disabled={request.download === null || request.download === ""}>DESCARGAR</Button>
         </div>
     )
 }
