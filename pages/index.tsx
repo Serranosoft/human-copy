52d23d3ef8d9501dbe76eb0de271ab098ac0ce13
Manuel Scholz <manuel.serrano943@gmail.com>
2022-07-06 18:51:49 +0200

    Fix: Retoques en los estilos de bigImg y pricing plans en landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,43 +36,46 @@ export default function Home({ products }: Props) {
 
 
                 <div className={s.bigImg}>
-                    <ClipSVG />
+                    <h2><span className="highlight-long">Estadísticas</span> de nuestros trabajos</h2>
                     <div>
-                        <div style={{ backgroundImage: "url(/sc-nt.jpg)" }}>
-                            <span>Página web <span className={s.highlight}>warez</span></span>
+                        <ClipSVG />
+                        <div>
+                            <div style={{ backgroundImage: "url(/sc-nt.jpg)" }}>
+                                <span>Página web <span className={s.highlight}>warez</span></span>
+                            </div>
+                            <span>+10.000% impresiones y clics en 7 meses</span>
                         </div>
-                        <span>+10.000% impresiones y clics en 7 meses</span>
-                    </div>
-                    <div>
-                        <div style={{ backgroundImage: "url(/sc-tst.jpg)" }}>
-                            <span>Página web de <span className={s.highlight}>series en inglés</span></span>
+                        <div>
+                            <div style={{ backgroundImage: "url(/sc-tst.jpg)" }}>
+                                <span>Página web de <span className={s.highlight}>series en inglés</span></span>
+                            </div>
+                            <span>+70% impresiones en el último mes</span>
                         </div>
-                        <span>+70% impresiones en el último mes</span>
-                    </div>
-                    <div>
-                        <div style={{ backgroundImage: "url(/sc-ce.jpg)" }}>
-                            <span>Página web de <span className={s.highlight}>criptomonedas</span></span>
+                        <div>
+                            <div style={{ backgroundImage: "url(/sc-ce.jpg)" }}>
+                                <span>Página web de <span className={s.highlight}>criptomonedas</span></span>
+                            </div>
+                            <span>+6000% impresiones con contenido en tendencia</span>
                         </div>
-                        <span>+6000% impresiones con contenido en tendencia</span>
-                    </div>
-                    <span>Pon el ratón por encima de las gráficas para conocer la <span className="highlight-medium">temática de cada proyecto</span></span>
-                    <div>
-                        <div style={{ backgroundImage: "url(/sc-db.jpg)" }}>
-                            <span>Página web de <span className={s.highlight}>recetas</span></span>
+                        <span>Pon el ratón por encima de las gráficas para conocer la temática de cada proyecto</span>
+                        <div>
+                            <div style={{ backgroundImage: "url(/sc-db.jpg)" }}>
+                                <span>Página web de <span className={s.highlight}>recetas</span></span>
+                            </div>
+                            <span>+55% impresiones y clics en los últimos 4 meses</span>
                         </div>
-                        <span>+55% impresiones y clics en los últimos 4 meses</span>
-                    </div>
-                    <div>
-                        <div style={{ backgroundImage: "url(/sc-hf.jpg)" }}>
-                            <span>Página web de <span className={s.highlight}>finanzas</span></span>
+                        <div>
+                            <div style={{ backgroundImage: "url(/sc-hf.jpg)" }}>
+                                <span>Página web de <span className={s.highlight}>finanzas</span></span>
+                            </div>
+                            <span>Crecimiento de un 40% trimestralmente</span>
                         </div>
-                        <span>Crecimiento de un 40% trimestralmente</span>
-                    </div>
-                    <div>
-                        <div style={{ backgroundImage: "url(/sc-idlt.jpg)" }}>
-                            <span>Página web de <span className={s.highlight}>noticias en prensa rosa</span></span>
+                        <div>
+                            <div style={{ backgroundImage: "url(/sc-idlt.jpg)" }}>
+                                <span>Página web de <span className={s.highlight}>noticias en prensa rosa</span></span>
+                            </div>
+                            <span>+1250% durante 2 meses y medio</span>
                         </div>
-                        <span>+1250% durante 2 meses y medio</span>
                     </div>
 
                 </div>
