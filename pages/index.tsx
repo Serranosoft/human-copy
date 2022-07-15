be0b1cbfadec078f39c0752bb88941af3de8ef7b
Manuel Scholz <manuel.serrano943@gmail.com>
2022-07-15 12:51:20 +0200

    Feat: Contenido y estilos landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,9 +79,33 @@ export default function Home({ products }: Props) {
                     </div>
 
                 </div>
+
+
+                <div className={s.description}>
+                    <h2>Lorem Ipsum <span className="highlight-long">dolor sit amet</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span>Donec neque enim</span>, elementum eu varius sed, vestibulum non tellus. Aenean massa nunc, fermentum ac enim vitae, porttitor interdum lectus. Integer et tortor laoreet, varius lacus nec, feugiat quam</p>
+                    <div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse suscipit elementum urna, sit amet efficitur neque.</span>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse suscipit elementum urna, sit amet efficitur neque.</span>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse suscipit elementum urna, sit amet efficitur neque.</span>
+                        </div>
+                    </div>
+                </div>
+
                 <Pricing
                     products={products}
                 />
+
+
+
                 <div className={s.stepByStep}>
                     <h1>¿Cómo funciona <span className="highlight-long">HumanCopy?</span></h1>
                     <div>
