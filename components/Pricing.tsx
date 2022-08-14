aec6bb13c8311834dfb23f0b1818b0519fa84748
Manuel Scholz <manuel.serrano943@gmail.com>
2022-08-14 13:18:03 +0200

    Feat: Animaciones, estilos

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -15,8 +15,6 @@ interface Props {
 
 export default function Pricing({ products }: Props) {
 
-    console.log(products);
-
     const router = useRouter();
     const [priceIdLoading, setPriceIdLoading] = useState<string>();
     const { user, isLoading, userDetails } = useUser();
