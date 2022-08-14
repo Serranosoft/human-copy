a04951b7569ef8dfb78c773fe83900c8ac0a4df0
Manuel Scholz <manuel.serrano943@gmail.com>
2022-08-14 18:48:36 +0200

    Feat: Descripción de Manuel Serrano Scholz

--- components/WhoWeAre.tsx
+++ components/WhoWeAre.tsx
@@ -28,8 +28,8 @@ export default function WhoWeAre() {
                         <div className={s.description}>
                             <h3>Manuel Serrano Scholz</h3>
                             <p>SEO & Programador</p>
-                            <span>Investigación, organización, esquematización y revisión. Soy la persona encargada de optimizar tu contenido para que consigas el máximo beneficio posible.</span>
-                            <span>Después de mucha experiencia redactando en mis proyectos quiero ayudar a crear contenidos de calidad y con proyección.</span>
+                            <span>Programador y SEO con 5 años de experiencia en total. Soy el encargado de brindar una plataforma que facilite el trabajo de webmasters y SEOs</span>
+                            <span>Tras desarrollar con éxito negocios digitales aplicando SEO generando miles de visitas, leads y remuneración he desarrollado junto con mi compañera esta plataforma para ayudarte a alcanzar tus objetivos</span>
                         </div>
                         <div className={s.socialBar}>
                             <a target="_blank" href="https://twitter.com/ImScholz"><img src="/twitter.svg" /></a>
